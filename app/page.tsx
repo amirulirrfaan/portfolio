80132cdf4db5f33c15298adffc03d66c97e577f3
amirulirrfaan <74703771+amirulirrfaan@users.noreply.github.com>
2024-11-22 11:37:50 +0800

    remove component and cange nextjs to 14

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,31 @@
+"use client";
+
 import About from "@/components/sections/about";
 import Contact from "@/components/sections/contact";
 import Experience from "@/components/sections/experience";
 import Hero from "@/components/sections/hero";
 import Projects from "@/components/sections/projects";
 import Skills from "@/components/sections/skills";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-4">
-      {/* <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      > */}
+    <>
       <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Experience />
-      <Contact />
-      {/* </motion.div> */}
-    </div>
+
+      <div className="container mx-auto px-4">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <About />
+          <Skills />
+          <Projects />
+          <Experience />
+          <Contact />
+        </motion.div>
+      </div>
+    </>
   );
 }
